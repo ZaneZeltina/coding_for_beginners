99fee93d2ca83da2cf42e76834694ca237ce1739
ZaneZeltina <zanezeltina32@inbox.lv>
2026-06-18 19:05:37 +0300

    Add Python file: puffy.py
    Add a new Python language file: puffy.py. That's a small interactive user greeting script that prints an intro, prompts for the user's name, and outputs a special message when the name is "Zane" or a default response otherwise.

---
+++ python/you_can_code/puffy.py
@@ -0,0 +1,8 @@
+print("Hello, my name is Puffy the Python!")
+
+name = input("What's your name? ")
+
+if name == "Zane":
+    print("Wow, that's a great name", name, "!")
+else:
+    print("Hmm, that's an odd name!")
