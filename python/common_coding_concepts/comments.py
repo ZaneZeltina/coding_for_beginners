6934ce29a92abe5e85f0339692413dee14479e91
Zane Zeltina <zanezeltina32@inbox.lv>
2026-06-24 19:30:12 +0300

    Add Python file: comments.py
    Add  a small interactive script demonstrating basic comments, input/output and a conditional branch. The script prompts for a user name, greets the user, asks if they want to know the program's name, and prints different responses.

---
+++ python/common_coding_concepts/comments.py
@@ -0,0 +1,18 @@
+"""
+This is a simple Python program that explores dialogues
+written by Zane Zeltina in 2026
+"""
+
+# First dialogue
+user_name = input("What's your name?")
+
+# Second dialogue
+print("Hello, " + user_name + "!")
+response = input("Do You want to know my name?")
+
+# Check user response for third dialogue
+if response == "y":
+    print("My name is Py!")
+else:
+    # print("Have a nice day!")
+    print("Ok, let's move on...")
