39c99006e4a72b5d0e8764fdbdde673e1c89d71d
Zane Zeltina <zanezeltina32@inbox.lv>
2026-07-04 21:22:58 +0300

    Add Python file: loops.py
    Add a new Python script with a simple `while` loop counter and a `for` loop over a shopping list to demonstrate basic loop syntax. Add some comments for better understanding.

---
+++ python/common_coding_concepts/loops.py
@@ -0,0 +1,19 @@
+"""Python counting script
+V1.0"""
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1 # This is the same as count plus one
+
+print("Finished counting!")
+
+"""Python for loop
+V1.0"""
+
+my_shopping_list = ["Apples", "Oranges", "Bread", "Milk", "Eggs"]
+
+for item in my_shopping_list:
+    print(item) # This will print each item in the list
+    
+print("Finished loop!")
