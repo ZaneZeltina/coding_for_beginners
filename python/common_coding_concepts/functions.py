ab1c4e9bcfbbcb0fd8c2241ae9654c8b79315640
Zane Zeltina <zanezeltina32@inbox.lv>
2026-07-10 18:45:41 +0300

    Add Python file: functions.py
    Add educational script demonstrating basic function concepts including function definition, parameters, function calls, and return values.

---
+++ python/common_coding_concepts/functions.py
@@ -0,0 +1,32 @@
+"""
+Write simple basic Python functions script
+"""
+
+def hello_world():
+    print("Hello World!")
+
+hello_world()
+
+# Example 2
+def say_hello(name):
+    print("Hello " + name + "!")
+
+say_hello("Zane")
+
+# Example 3
+def hi_message(name):
+    print("Hi, " + name + "!")
+
+person = "Zane"
+
+hi_message(person)
+
+# Example 4
+def greet_message(name):
+    greeting = "Hello, " + name + "!"
+    return greeting
+
+person = "Zane"
+
+message = greet_message(person)
+print(message)
